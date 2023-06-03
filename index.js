febcc0c043fa171e039e4c940871a2ae722b3410
Ballbuster <93219456+Ballbuster9000@users.noreply.github.com>
2023-06-03 13:22:00 +0300

    ooc messages

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
+let author = "tester guy"
 
 (async() => {
-    await characterAI.authenticateAsGuest();
+    await characterAI.authenticateWithToken(process.env.caitoken); // add this to .env file cuz guest is bad lol
 
     const characterId = "KMGd6dagzmsEL4nk6PW8x70YSB9oDjVBuCJN8RrQF8k"
 
     const chat = await characterAI.createOrContinueChat(characterId);
-    const response = await chat.sendAndAwaitResponse('hi joe', true)
+    let introduction = `(OOC: This message has been sent by ${author}.\n\n)`
+    let message = "hello" // actual message
+    let msg = introduction + message
+    
+    let response = await chat.sendAndAwaitResponse(msg, true)
+    // know's persons username now ig, have fun playing with this
 
     console.log(response.text);
 
